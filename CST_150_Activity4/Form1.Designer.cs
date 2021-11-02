87b9de2d6ecbcb8bfc064f88f8d5e923bf5d373c
Miguel Torres Perez <silverwolf1084@outlook.com>
2021-11-01 20:36:48 -0400

    Add project files.

---
+++ CST_150_Activity4/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CST_150_Activity4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.secLabel = new System.Windows.Forms.Label();
+            this.secTB = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // secLabel
+            // 
+            this.secLabel.AutoSize = true;
+            this.secLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secLabel.Location = new System.Drawing.Point(16, 11);
+            this.secLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.secLabel.Name = "secLabel";
+            this.secLabel.Size = new System.Drawing.Size(226, 25);
+            this.secLabel.TabIndex = 0;
+            this.secLabel.Text = "Enter time in seconds:";
+            // 
+            // secTB
+            // 
+            this.secTB.Location = new System.Drawing.Point(277, 11);
+            this.secTB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.secTB.Name = "secTB";
+            this.secTB.Size = new System.Drawing.Size(132, 22);
+            this.secTB.TabIndex = 1;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(13, 231);
+            this.calcButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(100, 64);
+            this.calcButton.TabIndex = 2;
+            this.calcButton.Text = "&Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(152, 231);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 64);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "C&lear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(287, 231);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(100, 64);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "&Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(177, 158);
+            this.resultLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 17);
+            this.resultLabel.TabIndex = 5;
+            this.resultLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(416, 308);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.secTB);
+            this.Controls.Add(this.secLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Seconds Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label secLabel;
+        private System.Windows.Forms.TextBox secTB;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ CST_150_Activity4/Form1.cs
@@ -0,0 +1,78 @@
+﻿/*Author: Miguel Torres Perez */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150_Activity4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            double seconds;
+            seconds = double.Parse(secTB.Text);
+            double days;
+            double hours;
+            double minutes;
+
+            try
+            {
+                if(seconds >= 86400)
+                {
+                    days = seconds / 86400;
+                    resultLabel.Text = days + " days";
+                }
+                else if(seconds >= 3600){
+                    hours = seconds / 3600;
+                    resultLabel.Text = hours + " hours";
+                }
+                else if(seconds >= 60)
+                {
+                    minutes = seconds / 60;
+                    resultLabel.Text = minutes + " minutes";
+                }
+                else
+                {
+                    resultLabel.Text = seconds + " seconds";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = " ";
+            secTB.Text = " ";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
